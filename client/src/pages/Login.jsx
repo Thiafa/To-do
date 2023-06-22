3a09acfe5d58d21235c9930585c12c3d86f300ab
Thiago <thiafa.vos@gmail.com>
2023-06-22 18:55:47 -0300

    Feat: Inserir tela de login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
+import imgBackground from '../assets/background.png';
+
 export const Login = () => {
-  return <div className="bg-slate-600">asdas</div>;
+  return (
+    <div
+      className="bg-slate-600 w-screen h-screen flex  items-center pl-[10rem] mt-"
+      style={{
+        backgroundImage: `url(${imgBackground}),linear-gradient(#D9D8D6, #EAE9E7 )`,
+        backgroundSize: 'auto',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor: 'linear-gradient(#D9D8D6, #EAE9E7 )',
+      }}
+    >
+      <div
+        className="rounded-full w-[350px] h-[450px] flex flex-col mt"
+        style={{
+          borderRadius: ' 35px',
+          background: ' rgba(214, 214, 214, 0.70)',
+          boxShadow: ' 2px 2px 10px 0px rgba(149, 149, 149, 0.25)',
+          backdropFilter: ' blur(17px)',
+        }}
+      >
+        <h1 className="text-2xl text-center mt-8" style={{}}>
+          Faça seu Login
+        </h1>
+      </div>
+    </div>
+  );
 };
